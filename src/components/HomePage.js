0de9043b027bc422c3e7b331285610429df51445
rtenhacken <rtenhacken@mobiquityinc.com>
2018-10-23 16:38:50 +0200

    created the HeaderComponent

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../public/App.css';
+import HeaderComponent from './HeaderComponent';
 
 class HomePage extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <HeaderComponent />
           <h1>New Mobster Album</h1>
         </header>
       </div>
